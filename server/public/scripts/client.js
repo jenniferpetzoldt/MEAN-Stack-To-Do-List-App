4888e77460bbb368e21d44baa0a7393a758b47e8
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-09 20:57:35 -0500

    mongo working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,5 @@ let listApp = angular.module('listApp', []);
 
 listApp.controller('ListController', function(){
     const vm = this;
-    vm.message = 'Hello!';
+   
 });
--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/lists';
 
+mongoose.connect(mongoURI, {useNewUrlParser: true});
+
 mongoose.connection.on('open', ()=>{
     console.log('Connected to Mongo');
 });
