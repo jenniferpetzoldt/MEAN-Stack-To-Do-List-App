b5082e1693e0a88205032712475c61294ac25b0e
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-09 20:30:58 -0500

    added gitignore

--- server/server.js
+++ server/server.js
@@ -2,27 +2,27 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const repairRouter = require('./routes/list.router.js');
+//const repairRouter = require('./routes/list.router.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //uses
 app.use(express.static('server/public'));
-app.use('/list', listRouter);
+//app.use('/list', listRouter);
 
 //globals
 const port = process.env.PORT || 5000;
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/lists';
+// const mongoose = require('mongoose');
+// const mongoURI = 'mongodb://localhost:27017/lists';
 
-mongoose.connection.on('open', ()=>{
-    console.log('Connected to Mongo');
-});
+// mongoose.connection.on('open', ()=>{
+//     console.log('Connected to Mongo');
+// });
 
-mongoose.connection.on('error', (error)=>{
-    console.log('ERROR CONNECTING TO MONGO', error);
-});
+// mongoose.connection.on('error', (error)=>{
+//     console.log('ERROR CONNECTING TO MONGO', error);
+// });
 
 //spin up server
 app.listen(port, ()=>{
