227581f790aa040f7b05921ad47096e4d60904fe
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-10 00:11:30 -0500

    POST is sending task to database

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -8,9 +8,23 @@ const TaskSchema = new Schema({
     completed: {type: Boolean}
 });
 
-const List = mongoose.model('Lists', ListSchema);
+const List = mongoose.model('Lists', TaskSchema);
 
 let list = [];
+
+router.post('/', (req, res)=>{
+    console.log('/list POST');
+    console.log(req.body);
+    let taskFromClient = req.body;
+    const taskToAdd = new List(taskFromClient);
+    taskToAdd.save().then(()=>{
+        console.log('Task added', taskToAdd);
+        res.sendStatus(201);
+    }).catch((error)=>{
+        console.log(error);
+        res.sendStatus(500);
+    });
+});
 
 
 module.exports = router;
